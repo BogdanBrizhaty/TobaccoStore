7af30d9b83555e539700f6fd123b383bc2e06bae
BogdanBrizhaty <demaskinas@yandex.ua>
2017-09-01 13:34:41 +0300

    client base

---
+++ TobaccoStore.Web/Client/RouteConfig.js
@@ -0,0 +1,10 @@
+﻿var routeConfig = app.config(['$routeProvider', ($routeProvider) => {
+    $routeProvider.when('/', {
+        templateUrl: 'Client/Views/Home.html',
+        Controller: 'HomeController'
+    });
+    $routeProvider.when('/tobacco/:id', {
+        // controller here
+        // temolate url here
+    });
+}]);
